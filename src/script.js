29560777305a943c305c48e7cc767c201278b6da
SinghSourabh07 <singhsourabh070819@gmail.com>
2022-09-13 22:04:52 +0530

    initial code 13-9-22

---
+++ src/script.js
@@ -0,0 +1,4 @@
+document.getElementById("p1").innerHTML = "Hello JavaScript";
+document.getElementById("p1").style.color = 'red';
+document.getElementById("p1").style.fontSize = '35px';
+document.getElementById("p1").style.backgroundColor = 'white'
